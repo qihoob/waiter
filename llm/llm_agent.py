49ea80bfc60bd325cd76dcd8d5e83fc2a160431b
tianzebing <675852092@qq.com>
2025-07-06 16:14:56 +0800

    增加测试代码

--- llm/llm_agent.py
+++ llm/llm_agent.py
@@ -11,7 +11,7 @@ from collector.prompt_builder.prompt import PromptBuilder
 # 构建内存管理对象，以获取用户历史对话记录
 memory_manager = RedisMemoryManager()
 # 构建提示词模板生成器
-#builder = PromptBuilder(max_length=512)
+builder = PromptBuilder(max_length=512)
 
 
 ds_model = ChatDeepSeek(
@@ -36,23 +36,24 @@ def build_recommend_agent(user_id, session_id, input_text, restaurant_id):
         verbose=True
     )
     # 构建提示词
-    #prompt = builder.build_prompt(input_text, user_id=user_id, location='location')
-    prompt='''
-        你是一个智能服务员，需要完成以下任务
-        -理解用户需求
-        -提供餐厅服务
-        -处理客户投诉
-        -推荐特色菜品
-        当前用户请求:
-            4人聚餐，来点香辣菜
-            当前城市为 北京，
-            推荐当地特色菜品如烤鸭, 炸酱面, 涮羊肉。
-        用户画像分析:
-            场景类型:聚餐
-            参与人数:4
-            -口味要求:香辣
-        历史点单记录:
-            无
-        请根据以上信息综合判断并提供服务。
-    '''
+    prompt = builder.build_prompt(input_text, user_id=user_id, location='location')
+    # 下边注释测试用
+    # prompt='''
+    #     你是一个智能服务员，需要完成以下任务
+    #     -理解用户需求
+    #     -提供餐厅服务
+    #     -处理客户投诉
+    #     -推荐特色菜品
+    #     当前用户请求:
+    #         4人聚餐，来点香辣菜
+    #         当前城市为 北京，
+    #         推荐当地特色菜品如烤鸭, 炸酱面, 涮羊肉。
+    #     用户画像分析:
+    #         场景类型:聚餐
+    #         参与人数:4
+    #         -口味要求:香辣
+    #     历史点单记录:
+    #         无
+    #     请根据以上信息综合判断并提供服务。
+    # '''
     return agent.run(prompt)
